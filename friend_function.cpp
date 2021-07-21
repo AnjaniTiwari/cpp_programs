fdd3a63789fde6d75ade0c1e47625c70432c2db8
AnjaniTiwari <86887873+AnjaniTiwari@users.noreply.github.com>
2021-07-21 17:23:03 +0530

    friend_function

---
+++ friend_function.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Complex
+{
+private:
+    int n1,n2;
+public:
+    void setData(int a,int b)
+    {
+        n1=a;
+        n2=b;
+    }
+    void showData()
+    {
+        cout<<n1<<" "<<n2<<endl;
+    }
+    friend void copyNo(Complex); //friend function declaration
+};
+void copyNo(Complex c)  //friend function definition
+{
+   cout<<"sum ="<<c.n1+c.n2;
+}
+int main()
+{
+    Complex c1;
+    c1.setData(1,2);
+    c1.showData();
+    copyNo(c1);     //friend function call
+
+}
